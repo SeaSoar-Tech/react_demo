16425bf99334c23254e3c62b457e09f77ea5627e
lichever <zhanghaixiang528@gmail.com>
2024-03-07 02:29:17 -0800

    add useContext about obj demo

--- react-basic/src/HooksTestUseContext.js
+++ react-basic/src/HooksTestUseContext.js
@@ -1,33 +1,47 @@
-import {useContext } from 'react';
-import {FruitsContext} from './App.js'; //不是default export, 所以要用{}包裹
+import { useContext, createContext, useState } from "react";
+import { FruitsContext } from "./App.js"; //不是default export, 所以要用{}包裹
 
 // 这个是用来测试useContext hook
 
-function Child(params) {
-
-    let fruits = useContext(FruitsContext);
-    console.log(fruits);
-    return (<div>
-
-            {fruits.map( (item, index)=>
-                 <p key={index}>{item}</p>
-            ) }
-        
-        </div>)
-
+function Child(props) {
+  let fruits = useContext(FruitsContext);
+  console.log(fruits);
+  return (
+    <>
+      <div>
+        {fruits.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+      <h3>grand child use obj context</h3>
+      <GrandChild />
+    </>
+  );
+}
+function GrandChild(props) {
+  const {count, setCount}= useContext(objContext);
+  return (
+    <div>
+        <p>{`In the grandchild level ->  show count: ${count}`}</p>
+        <button onClick={() => setCount(count + 1)}>increment</button>
+    </div>
+  );
 }
 
-
-
+const objContext = createContext(null);
 function HooksTestUseContext() {
-    
+  const [count, setCount] = useState(0);
 
-    return (<div>   
+  return (
+    <objContext.Provider value={{ count, setCount }}>
+      <div>
         <h2>useContext example</h2>
+        <p>{`In the grandfather level -> show count: ${count}`}</p>
+        <button onClick={() => setCount(count - 1)}>decrement</button>
         <Child />
-    </div>)
-
+      </div>
+    </objContext.Provider>
+  );
 }
-
 
 export default HooksTestUseContext;
