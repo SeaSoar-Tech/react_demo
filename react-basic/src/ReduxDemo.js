6011c1251f27e69e1674e0bd28435602bde8caca
SeaSoar-Tech <seasoartech@gmail.com>
2024-03-24 23:26:03 -0700

    add redux store

--- react-basic/src/ReduxDemo.js
+++ react-basic/src/ReduxDemo.js
@@ -9,8 +9,10 @@ export default function ReduxDemo(){
     return (
 
         <>
-            <h2>redux demo</h2>        
-        
+            <h2>redux demo</h2>       
+            <p>Count: {}</p> 
+            <button>+</button>
+            <button>-</button>
         </>
 
 
