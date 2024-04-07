1d7148fcd5b77828fda0feb87615d8753f6e3c29
SeaSoar-Tech <seasoartech@gmail.com>
2024-04-06 22:54:56 -0700

    test try-except-finally-raise

--- react-basic/src/test/test.py
+++ react-basic/src/test/test.py
@@ -1,3 +1,16 @@
 
 import string
+import sys
+
+try:
+    input = sys.argv
+    if len(input) != 2:
+        raise ValueError("input number is not right")
+    num = int(input[1])
+    print("num is ", num)
+except Exception as e:
+    print("exception is ", e)
+    
+    
+# 后面这里还是会执行的
 print(string.ascii_lowercase)
