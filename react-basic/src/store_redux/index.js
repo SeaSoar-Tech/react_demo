376958c69205620ac0c8d9a1486d749f6589beb4
SeaSoar-Tech <seasoartech@gmail.com>
2024-04-17 23:59:14 -0400

    change index.js for redux root store

--- react-basic/src/store_redux/index.js
+++ react-basic/src/store_redux/index.js
@@ -1,10 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import counterReducer from './modules/counterSlice'
-
+import channelReducer from './modules/channelSlice'
 
  const store = configureStore({
     reducer:{
-        counter : counterReducer
+        counter : counterReducer,
+        channel : channelReducer
     }
 
 })
